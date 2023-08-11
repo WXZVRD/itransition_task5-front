373da658b27ffe88b2293124290a8b177d6eb72b
WXZVRD <134217021+WXZVRD@users.noreply.github.com>
2023-08-11 17:26:52 +0600

    Update Axios.ts

--- src/Axios.ts
+++ src/Axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://64d61969c55b4555432892a2--melodic-alfajores-adbef2.netlify.app'
+    baseURL: 'https://melodic-alfajores-adbef2.netlify.app'
 });
 
 export default instance
