44c0d800ddfc760c5e82843d64ccec3fdbcb9835
WXZVRD <134217021+WXZVRD@users.noreply.github.com>
2023-08-11 17:57:41 +0600

    Update Axios.ts

--- src/Axios.ts
+++ src/Axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://itransition-task5-backend-wxzvrd.onrender.com'
+    baseURL: 'https://itransition-task5-backend-wxzvrd.onrender.com:3301'
 });
 
 export default instance
