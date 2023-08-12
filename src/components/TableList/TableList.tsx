c6a5329a681af1b3aeaa1c17bf64b30e9e44778e
WXZVRD <134217021+WXZVRD@users.noreply.github.com>
2023-08-12 12:06:30 +0600

    Update TableList.tsx

--- src/components/TableList/TableList.tsx
+++ src/components/TableList/TableList.tsx
@@ -1,65 +1,75 @@
-import { Box, CircularProgress, Skeleton } from "@mui/material";
-import TableRow from "../TableRow/TableRow";
 import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { fetchData, fetchMoreData } from "../../redux/slices/dataSlice";
-import InfiniteScroll from 'react-infinite-scroller';
+import {TableContainer, Paper, TableCell, TableRow, TableHead, CircularProgress, Box} from "@mui/material";
+import { setPage } from "../../redux/slices/configSlice";
+import { TableVirtuoso } from "react-virtuoso";
 
 const TableList = () => {
     const dispatch = useAppDispatch();
-    const config = useAppSelector(state => state.config);
-    const { list, status } = useAppSelector(state => state.data);
+    const config = useAppSelector((state) => state.config);
+    const { list, status } = useAppSelector((state) => state.data);
 
     const [num, setNum] = useState(2);
 
     const loadMore = () => {
-        setNum(num + 1);
-        dispatch(fetchMoreData({ iseed: config.seed, locale: config.locale, page: num }));
+        dispatch(fetchMoreData({ iseed: config.seed, locale: config.locale, page: num, failCount: config.failCount  }));
+        setNum((prevNum) => prevNum + 1);
     };
 
     useEffect(() => {
+        setNum(2)
         if (status !== "loading") {
             dispatch(fetchData({ iseed: config.seed, locale: config.locale, page: config.pageCount, failCount: config.failCount }));
         }
     }, [config]);
 
+    const rowContent = (index:any) => {
+        const row = list[index];
+        return (
+            <TableRow sx={{width:'3000px'}} key={index}>
+                <TableCell
+                    align={'left'}
+                    width='70px'
+                >{index}</TableCell>
+                <TableCell width='260px'>{row.fullName}</TableCell>
+                <TableCell width='290px'>
+                    {row.address.country} {row.address.city} {row.address.street}
+                </TableCell>
+                <TableCell width='300px'>{row.uid}</TableCell>
+                <TableCell width='300px'>{row.phone}</TableCell>
+            </TableRow>
+        );
+    };
+
+    const header = () => {
+        return (
+            <TableHead sx={{bgcolor:'white', width:'1150px', justifyContent:'space-between'}}>
+                <TableRow sx={{width:'1250px',}}>
+                    <TableCell >Num</TableCell>
+                    <TableCell width='300px'>Full Name</TableCell>
+                    <TableCell width='300px'>Address</TableCell>
+                    <TableCell width='300px'>UID</TableCell>
+                    <TableCell width='300px'>Phone</TableCell>
+                </TableRow>
+            </TableHead>
+        )
+    }
+
     return (
-        <Box>
-            {status === "loaded" ? (
-                <InfiniteScroll
-                    pageStart={0}
-                    loadMore={loadMore}
-                    hasMore={true}
-                    loader={<CircularProgress color="secondary" />}
-                    className="infinite-scroll"
-                >
-                    <Box
-                        sx={{
-                            display: 'grid',
-                            gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
-                            gap: '16px',
-                            width: '100%',
-                            padding: '16px',
-                        }}
-                    >
-                        {list.map((el, index) => (
-                            <TableRow
-                                key={index}
-                                index={index}
-                                fullName={el.fullName}
-                                uid={el.uid}
-                                address={el.address}
-                                phone={el.phone}
-                            />
-                        ))}
-                    </Box>
-                </InfiniteScroll>
-            ) : (
-                Array.from({ length: 20 }).map((_, index) => (
-                    <Skeleton key={index} width="100%" height="50px" />
-                ))
-            )}
-        </Box>
+        <TableContainer sx={{width:'1150px'}} component={Paper}>
+                <TableVirtuoso
+                    style={{ height: 500, width: "1150px", justifyContent:'space-between' }}
+                    data={Array.from({ length: list.length })}
+                    itemContent={rowContent}
+                    endReached={loadMore}
+                    fixedHeaderContent={header}
+                    components={{
+                        TableHead: TableHead,
+                        TableFoot: CircularProgress
+                    }}
+                />
+        </TableContainer>
     );
 };
 
